2ce812de97cb864d95b19160503aab90ed587437
Pythom1234 <pythom1234@email.cz>
2024-02-29 16:44:15 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,83 @@
 //% color=#157432 icon="\uf03e"
 namespace imageio {
     //% block="5×5"
+    //% weight=100
     //% subcategory="normal"
     //% shim=images::createImage
     //% imageLiteral=1 imageLiteralRows=5 imageLiteralColumns=5
     export function createImage5x5(leds: string): Image {
+        return <Image><any>leds
+    }
+    //% block="5×10"
+    //% weight=99
+    //% subcategory="normal"
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=5 imageLiteralColumns=10
+    export function createImage5x10(leds: string): Image {
+        return <Image><any>leds
+    }
+    //% block="10×10"
+    //% weight=98
+    //% subcategory="normal"
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=10 imageLiteralColumns=10
+    export function createImage10x10(leds: string): Image {
+        return <Image><any>leds
+    }
+    //% block="20×20"
+    //% weight=97
+    //% subcategory="normal"
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=20 imageLiteralColumns=20
+    export function createImage20x20(leds: string): Image {
+        return <Image><any>leds
+    }
+    //% block="30×30"
+    //% weight=100
+    //% subcategory="big"
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=30 imageLiteralColumns=30
+    export function createImage30x30(leds: string): Image {
+        return <Image><any>leds
+    }
+    //% block="40×40"
+    //% weight=99
+    //% subcategory="big"
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=40 imageLiteralColumns=40
+    export function createImage40x40(leds: string): Image {
+        return <Image><any>leds
+    }
+    //% block="50×50"
+    //% weight=98
+    //% subcategory="big"
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=50 imageLiteralColumns=50
+    export function createImage50x50(leds: string): Image {
+        return <Image><any>leds
+    }
+    //% block="16×32"
+    //% weight=100
+    //% subcategory="very big"
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=46 imageLiteralColumns=32
+    export function createImage16x32(leds: string): Image {
+        return <Image><any>leds
+    }
+    //% block="32×64"
+    //% weight=99
+    //% subcategory="very big"
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=32 imageLiteralColumns=64
+    export function createImage32x64(leds: string): Image {
+        return <Image><any>leds
+    }
+    //% block="64×128"
+    //% weight=98
+    //% subcategory="very big"
+    //% shim=images::createImage
+    //% imageLiteral=1 imageLiteralRows=64 imageLiteralColumns=128
+    export function createImage64x128(leds: string): Image {
         return <Image><any>leds
     }
 }
